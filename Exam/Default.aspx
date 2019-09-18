0b5a93ef4861c8e3c20e489adee8e13d8e3f8f18
Jamshid Massomy <jamshidmassomi@gmail.com>
2019-09-18 08:43:22 +0430

    Mac Setup

--- Exam/Default.aspx
+++ Exam/Default.aspx
@@ -61,30 +61,32 @@
         </div>
     </div>
     <div class="col-lg-6 col-md-6 ">
-        <div class="card">
+        <div class="card" runat="server" ID="QuestionBody">
             <div class="card-body" runat="server" id="Xm_Question">
                 <blockquote class="blockquote blockquote-bordered py-2 pl-3 mb-0">
-                    <p class="mb-2 font-size-lg" runat="server" ID="Question"></p>
+                    <p class="mb-0 font-weight-black" runat="server" ID="Question"></p>
+                    <br/>
                     <div class="form-check">
-                         <asp:CheckBox runat="server" ID="_1thBox" CssClass="form-check-input" Text="1rd Option"/>
+                         <asp:CheckBox runat="server" ID="_1thBox" CssClass="form-check-input" onClick="Validate(id)"  />
                     </div>
                     <br/>
                     <div class="form-check">
-                        <asp:CheckBox runat="server" ID="_2thBox" CssClass="form-check-input" Text="2rd Option"/>
+                        <asp:CheckBox runat="server" ID="_2thBox" CssClass="form-check-input"  onClick="Validate(id)" />
                     </div>
                     <br/>
                     <div class="form-check">
-                         <asp:CheckBox runat="server" ID="_3thBox" CssClass="form-check-input" Text="3rd Option"/>
+                         <asp:CheckBox runat="server" ID="_3thBox" CssClass="form-check-input"  onClick="Validate(id)" />
                     </div>
                     <br/>
                     <div class="form-check">
-                       <asp:CheckBox runat="server" ID="_4thBox" Text="Something" class="form-check-input"/>
+                       <asp:CheckBox runat="server" ID="_4thBox"  class="form-check-input" onClick="Validate(id)" />
                     </div>
                 </blockquote>
             </div>
         </div>
     </div>
     <div id="CaptureResult">
+        <input type="hidden" runat="server" id="HCounter"/>
         <input type="hidden" runat="server" id="HQID"/>
         <input type="hidden" runat="server" id="HCHID"/>
     </div>
@@ -97,8 +99,8 @@
                  <i class="icon-arrow-left15"></i>
              </a>
          </li>
-         <li class="page-item active"><a href="#" class="page-link page-link-white legitRipple">1</a></li>
-         <li class="page-item"><a href="#" class="page-link page-link-white legitRipple">2</a></li>
+         <li class="page-item active" runat="server" ID="Q1"><a href="#" class="page-link page-link-white legitRipple">1</a></li>
+         <li class="page-item" runat="server" ID="Q2"><a href="#" class="page-link page-link-white legitRipple">2</a></li>
          <li class="page-item"><a href="#" class="page-link page-link-white legitRipple">3</a></li>
          <li class="page-item"><a href="#" class="page-link page-link-white legitRipple">4</a></li>
          <li class="page-item"><a href="#" class="page-link page-link-white legitRipple">5</a></li>
@@ -117,7 +119,7 @@
  <div class="d-flex justify-content-center mt-3 mb-3">
  <ul class="list-inline mb-0">
      <li class="list-inline-item">
-         <asp:LinkButton runat="server" ID="Btn_Next" class="btn btn-primary ml-auto legitRipple" OnClick="Next">
+         <asp:LinkButton runat="server" ID="Btn_Next" class="btn btn-primary ml-auto legitRipple" OnClick="Next" OnClientClick="fnClassChange();">
              بعدی 
              <i class="icon-backward2 mr-3 icon-2x"></i>
          </asp:LinkButton>
@@ -125,6 +127,12 @@
      <li class="list-inline-item">
          <asp:LinkButton runat="server" ID="Btn_Confirm" class="btn btn-primary ml-auto legitRipple" OnClick="Save">
              تایید 
+             <i class="icon-checkmark4 mr-3 icon-2x"></i>
+         </asp:LinkButton>
+     </li>
+     <li class="list-inline-item">
+         <asp:LinkButton runat="server" ID="Btn_Exit" class="btn btn-primary ml-auto legitRipple" OnClick="Exit" Visible="False">
+             ختم
              <i class="icon-checkmark4 mr-3 icon-2x"></i>
          </asp:LinkButton>
      </li>
@@ -137,24 +145,28 @@
 </html>
 
 <script type="text/javascript">
-    //$('#Btn_Next').on({
-    //    click: function (e) {
-    //        e.preventDefault();
-    //        //for (var i = 10; i <= 10; i++) {
-    //        //    $('.page-item').removeClass('active');
-    //        //    $(this).addClass('active');
-    //        //}
-    //        $('.page-item').each(function() {
-    //            $(this).addClass('active');
-    //        });
-    //        //$('.page-item').addClass('active');
-    //        $(this).addClass('active');
-           
-    //    }
-    //});
-    //var _ul = document.getElementsByName("pagination shadow-1");
+    $('input:checkbox').click(function () {
+        $('input:checkbox').not(this).prop('checked', false);
+    });
+    $('#Btn_Confirm').on({
+        click: function() {
+            alert('Are you sure ');
+        }
+    });
+    function fnClassChange() {
+        //e.preventDefault();
+        $('li.page-item.active').removeClass('active');
+        //$(this).addClass("active");
+        // var $next;
+        // var $selected = $("li.page-item.active");
+        // $next = $selected.next('li');
 
-    //$('#Btn_Next').on('click', function () {
-    //    $(this).addClass('active').siblings().removeClass('active');
-    //});
+        // $next = $selected.find('li.page-item.active');
+        // $next.removeClass("active");
+        //$next.next('li').addClass('active');;
+        alert('client click');
+    }
+    
+    
+
 </script>
--- Exam/Default.aspx.cs
+++ Exam/Default.aspx.cs
@@ -6,30 +6,30 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using Spire.Pdf.General.Render.Font.OpenTypeFile;
 
 public partial class EMS_Default : System.Web.UI.Page
 {
-    private int CategoryID { get; set; }
-    //private object QuestionID { get; set; }
-    //private object ChoiceID { get; set; }
     private string ApplicantCode { get; set; }
-
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
         {
+            ViewState["CategoryID"] = 1;
             if (!Convert.ToBoolean(Session["IsAuthenticate"]))
             {
                 Response.Redirect("~/security/index/");
             }
             else
             {
-                GenerateQuestion(e);
                 GetUserInfo();
+                GenerateQuestion(sender,e);
             }
+
         }
+        
     }
     protected void GetUserInfo()
     {
@@ -46,10 +46,13 @@ public partial class EMS_Default : System.Web.UI.Page
         }
         User_Bio.InnerHtml = _html;
     }
-    protected void GenerateQuestion( object Val)
+    protected void GenerateQuestion(object o, EventArgs e)
     {
-        CategoryID++;
-        foreach (System.Data.DataRow row in plus.Data.DAL.GetTable("Default", "EXEC xm.spGetQuestions @CategoryID", "CategoryID", CategoryID).Rows)
+
+        
+        int CheckCounter = 0;
+        var _Boxes = new CheckBox[] { _1thBox, _2thBox, _3thBox, _4thBox };
+        foreach (System.Data.DataRow row in plus.Data.DAL.GetTable("Default", "EXEC xm.spGetQuestions @CategoryID", "CategoryID", ViewState["CategoryID"] = (int)(ViewState["CategoryID"])+1).Rows)
         {
             var Options = plus.Data.DAL.FindArrayList(plus.Data.DAL.GetSettings("Default"),
                 "SELECT * FROM xm.Choice where QuestionID = "+row["ID"]+" ");
@@ -61,48 +64,107 @@ public partial class EMS_Default : System.Web.UI.Page
             _4thBox.Text= Convert.ToString(Options[3][2]);
 
             HQID.Value = Convert.ToString(row["ID"]);
-            HCHID.Value = Convert.ToString(Options[0][0]);
-            //Options[0][2];
 
+            ViewState["Choice1"] = Options[0][0];
+            ViewState["Choice2"] = Options[1][0];
+            ViewState["Choice3"] = Options[2][0];
+            ViewState["Choice4"] = Options[3][0];
+            
 
-            ///OptionsID  [0][0] [1][0]/ [2][0] [3][0]
-            //QID = row["ID"];
-            //QuestionID = QID;
-            // ChID = Options[1][2];
         }
-       
+        if ((int)ViewState["CategoryID"] > 7 || (int)ViewState["CategoryID"] == 7)
+        {
+            Exit(o,e);
+        }
+
     }
 
     protected void Save(object o, EventArgs e)
     {
-        ///to save and return the reuslt from HCHID.Value and HQID.Value
-        Response.Write("Question ID :"+Convert.ToString(HQID.Value)+"and Check Box valu is :"+ Convert.ToString(HCHID.Value));
-         //Response.Write(row["ID"] + "_____" + Options[0][0] + "______" + Options[1][0] + "_____" + Options[2][0] + "___________" + Options[3][0]);
+        //int i = 1;
         if (_1thBox.Checked)
         {
-
+            Response.Write("first");
+            HCHID.Value =  ViewState["Choice1"].ToString();
+            Response.Write(HCHID.Value);
         }
         if (_2thBox.Checked)
         {
-
+            Response.Write("second");
+            HCHID.Value = ViewState["Choice2"].ToString();
+            Response.Write(HCHID.Value);
+            //HCHID.Value = ViewState["Choice1"].ToString();
         }
         if (_3thBox.Checked)
         {
-
+            Response.Write("third");
+            HCHID.Value = ViewState["Choice3"].ToString();
+            Response.Write(HCHID.Value);
+            //HCHID.Value = ViewState["Choice1"].ToString();
         }
         if (_4thBox.Checked)
         {
-
+            Response.Write("last");
+            //HCHID.Value = ViewState["Choice1"].ToString();
         }
+
+        //var _Boxes = new CheckBox[] { _1thBox, _2thBox, _3thBox, _4thBox };
+        //for (int b = 0; b <= 4; b++)
+        //{
+        //    if (_Boxes[b].Checked)
+        //    {
+        //        //Response.Write("selcted ITem is:Options" + Options[b][0]);
+        //        HCHID.Value = ViewState["Choice1"].ToString();
+        //    }
+        //}
+        ///to save and return the reuslt from HCHID.Value and HQID.Value
+        Response.Write("Question ID :"+Convert.ToString(HQID.Value)+"and Check Box valu is :"+ Convert.ToString(HCHID.Value));
+        
+        Btn_Confirm.Attributes.Add("onclick", "this.disabled=true;");
+        Response.Write("dbl clicked");
+        //Btn_Confirm.
+
+
+        //Response.Write(row["ID"] + "_____" + Options[0][0] + "______" + Options[1][0] + "_____" + Options[2][0] + "___________" + Options[3][0]);
+
         //Response.Write("Question ID: _"+QuestionID+"Choic ID is :"+ChoiceID+"Applicant OTP is :"+Session["OTP"] );
+        //Btn_Confirm.Attributes.Add("OnClick","return false");
     }
     protected void Next(object o, EventArgs e)
     {
-       
-        GenerateQuestion(o);
+        var _Boxes = new CheckBox[] { _1thBox, _2thBox, _3thBox, _4thBox };
+        foreach (var check in _Boxes)
+        {
+            if (check.Checked)
+            {
+                check.Checked = false;
+            }
+        }
+
+        //for (int n = 0; n <= _Boxes.Length; n++)
+        //{
+        //    _Boxes[n].Checked = false;
+        //}
+        GenerateQuestion(o,e);
+
         //Response.Redirect("Next.aspx");
     }
-   
+
+    protected void Exit(object o, EventArgs args)
+    {
+        Response.Redirect("Result.aspx");
+        Session["IsAuthenticate"] = false;
+        //Response.Write("Greter then 7");
+        ViewState["CategoryID"] = 0;
+        Btn_Exit.Visible = true;
+        //HtmlGenericControl html = new HtmlGenericControl();
+        //html.InnerHtml = @"ختم 
+        // <i class='icon - backward2 mr - 3 icon - 2x'></i>
+        //    ";
+
+        ////LinkBtn.Controls.Add(html);
+        //‌Btn_Next.Controls.Add(html);
+    }
 }
 
 
